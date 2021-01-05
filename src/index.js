cfa254eb6cf67e7540a1c2794f460a54b41853a2
Sara Cemal <you@example.com>
2021-01-05 11:59:48 -0500

    fetchclient created

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 /* DOM elements */
 const pantry = document.querySelector("div#pantry")
 const recipeForm = document.querySelector("#recipe-form")
+const likeBtn = document.querySelector(".like-btn")
+const dislikeBtn = document.querySelector(".dislike-btn")
 
 /* event listeners */
+
+likeBtn.addEventListener("click", event => {
+
+})
+
+
 // pantry.addEventListener("click", event => {
 
-//     if (event.target.matches("#like-btn")) {
+//     if (event.target.matches(".like-btn")) {
 //         } 
 //     else 
 // })
@@ -14,7 +22,7 @@ const recipeForm = document.querySelector("#recipe-form")
 //     event.preventDefault()
 
 //     const newRecipeObj = {
-        
+
 //     }
 // })
 
@@ -25,12 +33,13 @@ const recipeForm = document.querySelector("#recipe-form")
 function renderIngredient(ingredientObj) {
     const ingredientsUl = document.querySelector(".snack-ingredients")
     const ingredientsLi = document.createElement("li")
-        ingredientsLi.textContent = ingredientObj
-        ingredientsUl.append(ingredientsLi)
+    ingredientsLi.textContent = ingredientObj
+    ingredientsUl.append(ingredientsLi)
 
 }
 
 function renderSnacks(snackObj) {
+    const snackDiv = document.querySelector("#snack-card")
     const img = document.querySelector(".pantry-image")
     const snackName = document.querySelector(".snack-name")
     const bioP = document.querySelector(".bio")
@@ -40,15 +49,18 @@ function renderSnacks(snackObj) {
     img.src = snackObj.image_url
     snackName.textContent = snackObj.name
 
+    snackDiv.append(img, snackName, )
+
     snackObj.recipe.forEach(ingredient => {
         renderIngredient(ingredient)
     })
 }
 
 
+
 /* fetch functions */
 
-function 
+// function 
 
 function getSnacks() {
     fetch("http://localhost:3000/api/v1/snacks")
@@ -63,6 +75,13 @@ function getSnacks() {
 
 
 
+
 // /* initialize */
 
-getSnacks()
+// getSnacks()
+client.get("snacks/")
+    .then(snackArray =>
+        snackArray.forEach(snackObj => {
+            renderSnacks(snackObj)
+        })
+    )
