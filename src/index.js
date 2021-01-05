4c2ccf994d27a065c137824a26b7e2f76c47af34
Sara Cemal <you@example.com>
2021-01-04 22:57:51 -0500

    ingredients, added form, HTML

--- src/index.js
+++ src/index.js
@@ -1,27 +1,54 @@
 /* DOM elements */
+const pantry = document.querySelector("div#pantry")
+const recipeForm = document.querySelector("#recipe-form")
 
+/* event listeners */
+// pantry.addEventListener("click", event => {
+
+//     if (event.target.matches("#like-btn")) {
+//         } 
+//     else 
+// })
+
+// recipeForm.addEventListener("submit", event => {
+//     event.preventDefault()
+
+//     const newRecipeObj = {
+        
+//     }
+// })
 
 
 
 /* render functions */
+
+function renderIngredient(ingredientObj) {
+    const ingredientsUl = document.querySelector(".snack-ingredients")
+    const ingredientsLi = document.createElement("li")
+        ingredientsLi.textContent = ingredientObj
+        ingredientsUl.append(ingredientsLi)
+
+}
+
 function renderSnacks(snackObj) {
     const img = document.querySelector(".pantry-image")
     const snackName = document.querySelector(".snack-name")
     const bioP = document.querySelector(".bio")
     bioP.textContent = snackObj.bio
 
-   
-
     img.dataset.id = snackObj.id
     img.src = snackObj.image_url
     snackName.textContent = snackObj.name
-        // bioHeader.createElem
 
-
+    snackObj.recipe.forEach(ingredient => {
+        renderIngredient(ingredient)
+    })
 }
 
 
 /* fetch functions */
+
+function 
 
 function getSnacks() {
     fetch("http://localhost:3000/api/v1/snacks")
